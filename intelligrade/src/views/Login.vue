58f1f512fe97443d754e481691bec23f2f5e7a1b
Ushyne <ushyneesclamado@gmail.com>
2025-08-14 10:49:12 +0800

    start

---
+++ intelligrade/src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login-container">
+      <h1>Welcome to IntelliGrade</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      <p class="signup-text">
+        Don’t have an account?
+        <router-link to="/signup">Create one</router-link>
+      </p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .login-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+  }
+  input, button {
+    margin: 8px 0;
+    padding: 10px;
+  }
+  .signup-text {
+    margin-top: 12px;
+  }
+  </style>
+  
