344b2e995ff8b6aada979702e169dbc5bcd131cc
jusalyngimao <jusalyng@gmail.com>
2025-11-11 00:19:15 +0800

    fix: resolve duplicate help button issue

--- intelligrade/src/views/student/Settings.vue
+++ intelligrade/src/views/student/Settings.vue
@@ -144,14 +144,6 @@
       </div>
     </div>
 
-    <!-- Floating Help & Support Button -->
-    <button @click="openHelpModal" class="floating-help-btn" title="Help & Support">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <circle cx="12" cy="12" r="10"/>
-        <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/>
-        <line x1="12" y1="17" x2="12.01" y2="17"/>
-      </svg>
-    </button>
 
     <!-- Help & Support Modal -->
     <div v-if="showHelpModal" class="modal-overlay" @click="closeHelpModal">
